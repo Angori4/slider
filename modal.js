d16920aec42887a8d9ecef77d1017c9d8158ecf9
Angori4 <110497521+Angori4@users.noreply.github.com>
2023-02-19 01:38:36 +0200

    add file

--- modal.js
+++ modal.js
@@ -1,23 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let slider = this.getElementById("brightness");
+    let slider = document.getElementById("brightness");
     slider.addEventListener("input", adjustSlider);
     adjustSlider(slider);
-});
-function adjustSlider(e) {
+  });
+  
+  function adjustSlider(e) {
     let body = document.body,
-    switchLightMin = 40
-    switchLightMax = 100
-    tar = e.target || e,
-    pot =+tar.value / +tar.max,
-    L1 = pot * (switchLightMax - switchLightMin) + switchLightMin,
-    L2 = L1 - 10,
-    L3 = L1 -37,
-    L = [L1, L2, L3],
-    thumbHueMin = 0,
-    thumbHueMax = 120,
-    thumbHue = pot * (thumbHueMax - thumbHueMin) + thumbHueMin,
-    thumbSat = 90.4,
-
+        switchLightMin = 40,
+        switchLightMax = 100,
+        tar = e.target || e,
+        pot = +tar.value / +tar.max,
+        L1 = pot * (switchLightMax - switchLightMin) + switchLightMin,
+        L2 = L1 - 10,
+        L3 = L1 - 37,
+        L = [L1, L2, L3],
+        thumbHueMin = 0,
+        thumbHueMax = 120,
+        thumbHue = pot * (thumbHueMax - thumbHueMin) + thumbHueMin,
+        thumbSat = 90.4,
+        thumbHSL = `${thumbHue},${thumbSat}%`
+  
     body.style.setProperty(`--p`, `hsl(${thumbHSL})`);
     body.style.setProperty(`--pT`, `hsla(${thumbHSL},0)`);
-}
+  }
